c6c202806c9e03f8547a152281b5a641ce8e556a
Ian Manix <ian.manix@wsu.edu>
2023-02-09 18:33:07 -0800

    cleaned up code, added documentation, TODOs, etc.

--- src/graphics/menu.js
+++ src/graphics/menu.js
@@ -1,6 +1,18 @@
 class Menu{
-	/**
+	/** Menu class
+	 *  this is a constructor for menus
 	 * 
+	 * TODO: add method to set menu position via coords in webgl space
+	 * 		https://webglfundamentals.org/webgl/lessons/webgl-text-html.html
+	 * TODO: Check mark buttons
+	 * TODO: sliders
+	 * TODO: text field
+	 * TODO: extra dvs (maybe)
+	 * 
+	 * TODO: test page to showcase/test different methods
+	 * 
+	 * @param {String} element name
+	 * @param {String} content div name
 	 * 
 	 */
 	constructor(name='menu', content_name='menu_content') {
@@ -11,6 +23,8 @@ class Menu{
 		setTimeout(() => {
 			this.elem.classList.add('open');
 		}, 400);  // wait 400ms to ensure fades it
+
+		this.is_open = true;
 
 		this.content_elem = document.createElement('div');
 		this.content_elem.classList.add(content_name);
@@ -24,6 +38,12 @@ class Menu{
 
 	/**
 	 * add a button to the menu
+	 * @param {String} name
+	 * @param {String} text
+	 * @param {function} action on button press
+	 * @param {void} argument to pass on press
+	 * 
+	 * @return {Object} button object
 	 */
 	add_button(name, text, on_press, button_arg) {
 		const button_elem = document.createElement('button');
@@ -34,12 +54,20 @@ class Menu{
 			on_press(button_arg);
 		});
 
-		document.body.appendChild(button_elem);
+		this.content_elem.appendChild(button_elem);
+		return button_elem;
 	}
 
 	/**
 	 * special exit menu button
 	 * defaults to x button in top corner, but can be specified elsewhere as well
+	 * 
+	 * @param {String} name
+	 * @param {String} text
+	 * @param {function} action on button press
+	 * @param {void} argument to pass on press
+	 * 
+	 * @return {Object} button object
 	 */
 	add_close_button(name, text, on_press, button_arg) {
 		const button_elem = document.createElement('button');
@@ -57,7 +85,6 @@ class Menu{
 			});
 		}
 
-		//document.body.appendChild(button_elem);
 		this.content_elem.appendChild(button_elem);
 		return button_elem;
 	}
@@ -76,10 +103,13 @@ class Menu{
 		lable_elem.appendChild(input_elem);
 
 		this.content_elem.appendChild(input_elem);
+		return input_elem;
 	}
 
 	/**
 	 * add a text block to the menu
+	 * @param {String} name of block for css
+	 * @param {String} content of text block
 	 */
 	add_text(name, content) {
 		const text_elem = document.createElement('p');
@@ -87,6 +117,7 @@ class Menu{
 		text_elem.textContent = content;
 
 		this.content_elem.appendChild(text_elem);
+		return text_elem;
 	}
 
 
@@ -97,36 +128,73 @@ class Menu{
 		document.body.appendChild(this.elem);
 	}
 
-	/** closes modal
+	/** closes menu
+	 * FIXME: should this be private?
 	 */
 	close() {
 		this.elem.classList.remove('open');
 		setTimeout(() => {
 			document.body.removeChild(this.elem)
 		}, 400);  // let things fade out before removing them
+		this.is_open = false;
 	}
 
+	/** open menu if closed
+	 * so you don't need to re-make it every time it's called
+	 */
+	open() {
+		if (this.is_open) {
+			console.log("menu already open");
+		} else {
+			setTimeout(() => {
+				this.elem.classList.add('open');
+			}, 400);  // wait 400ms to ensure fades it
+
+			this.is_open = true;
+		}
+	}
+
+	/**
+	 * constructor to create a modal style menu
+	 * 
+	 * the element's name will be modal for css purposes
+	 * this turns a base menu class into a modal
+	 * TODO: make static constructor
+	 * IMPORTANT: do not call on menu that has been given elements already
+	 * 
+	 * @param {String} title text
+	 * @param {String} text
+	 * @param {String} confirm button text
+	 * @param {String} reject button text
+	 * 
+	 * @return {Promise}
+	 */
 	open_modal(title, text, confirm_text, reject_text) {
 		return new Promise ((resolve, reject) => {
-			this.create_open_modal(resolve, reject, title, text, confirm_text, reject_text)
+			this.#create_open_modal(resolve, reject, title, text, confirm_text, reject_text)
 		});
 	}	
 
-	create_open_modal(on_confirm, on_cancle, title_text, content_text, confirm_text, reject_text) {
-		//let modal = new Menu('modal', 'modal_content');
-		/*setTimeout(() => {
-			modal.elem.classList.add('open');
-		}, 400); */  // wait 400ms to ensure fades it
-
+	/**
+	 * method that dose the work of closing the modal
+	 * private
+	 * ref: https://www.youtube.com/watch?v=kJPkCGtv3vY
+	 * 
+	 * @param {Function} on confirm
+	 * @param {Function} on reject
+	 * @param {String} title text
+	 * @param {String} text
+	 * @param {String} confirm button text
+	 * @param {String} reject button text
+	 */
+	#create_open_modal(on_confirm, on_cancle, title_text, content_text, confirm_text, reject_text) {
 		// TODO: funcs to add element should specify what element to return it to, or return element to add
 
 		this.add_text('titleText', title_text);
 		this.add_text('messageText', content_text);
 		let elem = this.add_close_button('confirmText', confirm_text, on_confirm, 'Confirmed');
-		this.content_elem.appendChild(elem);
 		if (reject_text) {
 			let elem = this.add_close_button('cancelText', reject_text, on_cancle, 'Cancelled');
-			this.content_elem.appendChild(elem);
 		}
 
 		this.draw();
