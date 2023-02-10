3b53e50059347463be5f4bf6833664fa671159c1
Ian Manix <ian.manix@wsu.edu>
2023-02-09 21:08:04 -0800

    added test inputs

--- src/graphics/test.js
+++ src/graphics/test.js
@@ -1,7 +1,24 @@
 // make modal
 
 const modal = new Menu('modal', 'modal_content');
+const menu = new Menu('menu', 'menu_content');
 
+function on_press() {
+	console.log('clicked');
+}
+
+menu.add_button('1-1', '1-2', on_press, '1-3');
+menu.add_text('texttext', 'texttext');
+menu.add_checkbox('checkbox', 'checkbox');
+menu.add_slider('slider', 'slider');
+menu.add_textbox('textbox', 'textbox');
+menu.draw();
+
+function reopen(menu) {
+	console.log("reopening");
+	sleep(2);
+	menu.open();
+}
 
 document
 	.getElementById('openModal')
@@ -14,6 +31,6 @@ document
 			"TestText",
 			"Yes!",
 			"No?")
-			.then(value => console.log('Confirmed: ', value))
+			.then(value => console.log('Accepted ', value))
 			.catch(value => console.log('Rejected: ', value));
 	});
