b040b77f25297c4ccd67eca3e28360e62ddc4b2e
Ian Manix <ian.manix@wsu.edu>
2023-02-10 13:24:27 -0800

    added example menu output

--- src/graphics/test.js
+++ src/graphics/test.js
@@ -7,12 +7,31 @@ function on_press() {
 	console.log('clicked');
 }
 
-menu.add_button('1-1', '1-2', on_press, '1-3');
+const tbutton = menu.add_button('test_button', 'count', on_press, true);
 menu.add_text('texttext', 'texttext');
-menu.add_checkbox('checkbox', 'checkbox');
-menu.add_slider('slider', 'slider');
-menu.add_textbox('textbox', 'textbox');
+const checkbox = menu.add_checkbox('checkbox', 'checkbox');
+const slider = menu.add_slider('slider', 'slider', 10, 0, 20);
+const textbox = menu.add_input_textbox('textbox', 'textbox');
+//menu.add_text_element('textbox_lable', 'this is a text box', 'textbox');
+menu.add_close_button('close', 'X',on_press, false);
+
+let count = 0;
+// globals are bad, but make for easier testing here
+function test_input(count) {
+	count++;
+	console.log(count);
+	console.log("checkbox: ", checkbox.value);
+	console.log("slider: ", slider.value);
+	console.log("textbox: ", textbox.value);
+	//return count; doesn't work
+}
+
+menu.add_text('text', 'press to get state of inputs');
+let a = menu.add_button('print_button', 'Test!', test_input, count);
+
 menu.draw();
+
+
 
 function reopen(menu) {
 	console.log("reopening");
