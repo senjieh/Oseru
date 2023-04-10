8d41559f8d15a4b69e6a7f6536f0ae7a9035e7c9
Tom Freeman <thomas.freeman@wsu.edu>
2023-04-09 21:13:17 -0700

    updates
    updated readme for integration checklist
    fixed and issue with the local leaderboard

--- src/leaderboards/scoreboard.js
+++ src/leaderboards/scoreboard.js
@@ -1,16 +1,21 @@
 // Load scores from local storage
-//const FinalScore = require('./test.js');
 let level_name = "SilentNight";
 let level_key = level_name + "-scores";
 let scores = JSON.parse(localStorage.getItem(level_key)) || [];
-
+let 
 let level_title = level_name + " Scoreboard";
 document.querySelector("h1").textContent = level_title;
+
+// Refresh the scoreboard table initially
+refreshScoreboard();
+
+// Add an event listener to the clear button
+document.getElementById("clear-button").addEventListener("click", clearBoard);
+
 // Add a listener for the form submission
 document.getElementById("score-form").addEventListener("submit", function(event) {
   event.preventDefault(); // Prevent the form from submitting
-// Add an event listener to the clear button
-document.getElementById("clear-button").addEventListener("click", clearBoard);
+
   // Get the name and score and date
   let name_input = document.getElementById("name-input");
  
@@ -44,47 +49,48 @@ document.getElementById("clear-button").addEventListener("click", clearBoard);
     // Clear the name input field
     document.getElementById("score-form").remove();
   
-    // Refresh the scoreboard table
+    // Refresh the scoreboard table with the updated scores
     refreshScoreboard();
   });
-  function clearBoard(){
-      scores = [];
-      localStorage.setItem(level_key, JSON.stringify(scores));
-      refreshScoreboard();
-  }
-  // Function to refresh the scoreboard table
-  function refreshScoreboard() {
-    let tbody = document.querySelector("#scoreboard tbody");
-  
-    // Remove all existing rows from the table
-    while (tbody.firstChild) {
-      tbody.removeChild(tbody.firstChild);
-    }
-  
-    // Add rank, name, score, date into rows
-    scores.forEach(function(score, index) {
-      let tr = document.createElement("tr");
-      let rankTd = document.createElement("td");
-      let nameTd = document.createElement("td");
-      let scoreTd = document.createElement("td");
-      let dateTd= document.createElement("td");
-  
-      rankTd.textContent = index + 1;
-      nameTd.textContent = score.name;
-      scoreTd.textContent = score.score;
-      dateTd.textContent = score.date;
-  
-      tr.appendChild(rankTd);
-      tr.appendChild(nameTd);
-      tr.appendChild(scoreTd);
-      tr.appendChild(dateTd);
-      tbody.appendChild(tr);
-    });
-  }
-  
-  
-  // Initial refresh of the scoreboard table
+});
+//Clears all scores and players from the board
+function clearBoard(){
+  scores = [];
+  localStorage.setItem(level_key, JSON.stringify(scores));
   refreshScoreboard();
+}
+
+// Function to refresh the scoreboard table
+function refreshScoreboard() {
+  let tbody = document.querySelector("#scoreboard tbody");
+
+  // Remove all existing rows from the table
+  while (tbody.firstChild) {
+    tbody.removeChild(tbody.firstChild);
+  }
+
+  // Add rank, name, score, date into rows
+  scores.forEach(function(score, index) {
+    let tr = document.createElement("tr");
+    let rank_td = document.createElement("td");
+    let name_td = document.createElement("td");
+    let score_td = document.createElement("td");
+    let date_td= document.createElement("td");
+
+    rank_td.textContent = index + 1;
+    name_td.textContent = score.name;
+    score_td.textContent = score.score;
+    date_td.textContent = score.date;
+
+    tr.appendChild(rank_td);
+    tr.appendChild(name_td);
+    tr.appendChild(score_td);
+    tr.appendChild(date_td);
+    tbody.appendChild(tr);
   });
+}
+
+  
+  
   
 
